ba66f37b10a4acc23a64e80a61616cb5ebb7a219
Shrawan-git <shrawanshaky3@gmail.com>
2020-12-08 08:47:53 +0545

    shop list style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+      <Route exact path = '/' component={HomePage} /> 
+      <Route path = '/shop' component={ShopPage}/>
+      </Switch>
     </div>
+
   );
 }
 
