6777451edb992fb72f28289eb9363c6e8a08aa00
Shrawan-git <shrawanshaky3@gmail.com>
2020-12-10 23:28:02 +0545

    sign in page design completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path = '/' component={HomePage} /> 
       <Route path = '/shop' component={ShopPage}/>
+      <Route path = '/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import SHOP_DATA from './shop.data.js';
 import CollectionPreview from '../../components/collection-preview/collection-preview'
- 
+
 class ShopPage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state ={
+        this.state = {
             collections: SHOP_DATA
         }
     }
-    render(){
-        const {collections} = this.state;
+    render() {
+        const { collections } = this.state;
         return (<div className='shop-page'>
             {
-collections.map(({id, ...otherCollectionProps}) => (
-    <CollectionPreview key={id} {...otherCollectionProps} />
-))
+                collections.map(({ id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))
             }
-
-        </div>);
+        </div>
+        );
     }
 }
 export default ShopPage;
